70a5dcc05be605a455909e0158eeedeafc577595
Manish Kshetrapal <manishkshetrapal17@gmail.com>
2025-04-07 21:41:45 +0530

    [dev] first Lecture

---
+++ firstLecture.js
@@ -0,0 +1,40 @@
+console.log("Lecture 1");
+
+// var a = "str";
+// var b = 1;
+// var c = true;
+
+// console.log(a);
+// console.log(b);
+// console.log(c);
+
+// console.log(typeof a);
+// console.log(typeof b);
+// console.log(typeof c);
+
+// var d;
+// console.log(d);
+// console.log(typeof d);
+
+// var e = null;
+// console.log(typeof e);
+
+// var a = 2;
+// var b = 2;
+// console.log(a == b);
+
+// console.log(a === b);
+
+// console.log(2 + "abcd"); // Implicit numeric conversion
+
+// console.log(-"2" + "2");
+
+var a = [1, 2, "hello", true];
+
+var b = a;
+
+b.push("a");
+
+var a = [];
+console.log(a);
+console.log(b);
